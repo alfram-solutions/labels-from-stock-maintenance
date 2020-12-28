1f4904f2cf30c21c4b0894251f97c392f834215a
= <jaskooner@me.com>
2020-12-28 12:47:28 +0000

    Database Connection properties bean setup.
    DBProps.java bean to record database connection properties.

--- src/main/java/labels_from_stock_maintenance/DBProps.java
+++ src/main/java/labels_from_stock_maintenance/DBProps.java
@@ -1,4 +1,36 @@
 package labels_from_stock_maintenance;
 
 public class DBProps {
+
+    private String username;
+    private String password;
+    private String dbUrl;
+
+
+    public DBProps() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public void setDbUrl(String dbUrl) {
+        this.dbUrl = dbUrl;
+    }
 }
