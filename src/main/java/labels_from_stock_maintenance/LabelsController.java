6228e7b59847dd83e6bd5a185da7ff08d11fdcb8
alfram-solutions <jaskooner@alframsolutions.com>
2020-12-28 18:07:34 +0000

    Full mockup of Labels form
    Unable to find a javafx date and time picker
    so will create a separate dialogue to choose
    time.
    Added text and buttons to form to select time
    dialog.
    List view to display date items added
    List view to display product items
    Button to export product items to csv file
    Check box to de/select all product items

--- src/main/java/labels_from_stock_maintenance/LabelsController.java
+++ src/main/java/labels_from_stock_maintenance/LabelsController.java
@@ -14,6 +14,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LabelsController implements Initializable {
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module hellolabels {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
+    requires com.jfoenix;
 
     opens labels_from_stock_maintenance to javafx.fxml;
     exports labels_from_stock_maintenance;
